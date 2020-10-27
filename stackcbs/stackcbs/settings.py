c883dfd5b4cbf4e72a7d6c0fff9c1a55cb5ce872
aprajita0523 <52560938+aprajita0523@users.noreply.github.com>
2020-10-27 13:52:07 +0530

    Update settings.py
    data base setting updated

--- stackcbs/stackcbs/settings.py
+++ stackcbs/stackcbs/settings.py
@@ -81,22 +81,14 @@ WSGI_APPLICATION = 'stackcbs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'StackCbsDB',
-        'USER' : 'postgres',
-        'PASSWORD':'Appukuku@523',
-        'HOST':'localhost',
-    }
-}
 
-'''DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
